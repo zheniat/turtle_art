727206618b7d32455e3836374cbb192340ab7b6b
Eugene Citrisky <zheniat@Olenas-MacBook-Pro.local>
2022-05-02 18:39:41 -0700

    Turtle art template

---
+++ art.py
@@ -0,0 +1,16 @@
+import turtle
+
+t = turtle.Turtle()
+
+screen = turtle.Screen()
+screen.title('Red Cross')
+screen.setup(1000,1000)
+t.speed(0)
+t.hideturtle()
+t.color('red')
+
+# Write your code here
+
+
+#end of your code
+turtle.exitonclick()
